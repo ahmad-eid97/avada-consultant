ed0d03085c0858f5a058fa61c57ecaba6fe131bd
civil.ahmad.eid <civil.ahmad.eid@gmail.com>
2022-08-30 21:43:49 +0200

    merge

---
+++ dist/_nuxt/static/1661888579/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fabout","\u002Fcontact","\u002Flogin","\u002Fblogs","\u002Fforget","\u002Fpolicy","\u002Ffaq","\u002Fteam","\u002Fsoon","\u002Fterms","\u002Ftestimonials","\u002Fservices","\u002Fregister","\u002Fblog","\u002Fhome","\u002Fservice"]})
